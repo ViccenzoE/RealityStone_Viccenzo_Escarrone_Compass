d2a8c320458d83a3c0942e6e65d7ebbcdcb06576
Viccenzo Escarrone <viccenzoescarrone@gmail.com>
2022-07-28 11:17:19 -0300

    Adicionado casos de teste de subtração

--- Calculadora/specs/calculadora_spec.rb
+++ Calculadora/specs/calculadora_spec.rb
@@ -23,4 +23,26 @@ describe('Calculadora') do
     it("Deve somar decimais e inteiros") do
         expect(calculadora.soma(10, -1.2)).to eq 8.8
     end
+
+    
+
+    it('Deve subtrair dois inteiros positivos') do
+        expect(calculadora.subtrair(6, 1)).to eq 5
+    end
+
+    it("Deve subtrair números negativos") do
+        expect(calculadora.subtrair(-1, -1)).to eq 0
+    end
+
+    it("Deve subtrair dois decimais") do
+        expect(calculadora.subtrair(3.3, 3.4)).to eq -0.1
+    end
+
+    it("Deve subtrair dois decimais negativos") do
+        expect(calculadora.subtrair(-2.5, -1.1)).to eq -1.4
+    end
+
+    it("Deve subtrair decimais e inteiros") do
+        expect(calculadora.subtrair(10, 3.5)).to eq 6.5
+    end
 end
--- Calculadora/src/calculadora.rb
+++ Calculadora/src/calculadora.rb
@@ -3,5 +3,11 @@ class Calculadora
     def soma(a, b)
         a + b
     end
+
+    def subtrair(a, b)
+      subtracao =  a - b
+      subtracao.round(1)
+    end
+    
 end
     
